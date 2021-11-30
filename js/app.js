20ed418ae937b5e1e8ce1ec5697cbc37f90607ca
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-11-30 12:26:11 +0100

    [ADD] - Homepage (partial)

---
+++ js/app.js
@@ -0,0 +1,42 @@
+/* #Header
+  ======================================================= */
+const header = document.querySelector('.header');
+
+// Scroll
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    header.classList.add('header-white');
+  } else {
+    header.classList.remove('header-white');
+  }
+});
+
+if (window.scrollY > 50) {
+  header.classList.add('header-white');
+} else {
+  header.classList.remove('header-white');
+}
+
+/* #Posts Slider
+  ======================================================= */
+const postsSliders = document.querySelectorAll('.posts-slider');
+
+if (postsSliders) {
+  postsSliders.forEach((slider) => {
+    const sliderEl = slider.querySelector('.swiper');
+    const nextArrow = slider.querySelector('.next');
+    const prevArrow = slider.querySelector('.prev');
+
+    new Swiper(sliderEl, {
+      loop: true,
+      slidesPerView: 4,
+      navigation: {
+        nextEl: nextArrow,
+        prevEl: prevArrow,
+      },
+    });
+  });
+}
+
+
+  
