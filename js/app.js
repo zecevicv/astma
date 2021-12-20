a29106ea730b16e9e2f09300cfdb331b3f92bc54
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-12-20 16:14:58 +0100

    [ADD] - Search Popup

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 /* #Header
   ======================================================= */
 const header = document.querySelector('.header');
+const body = document.querySelector('body');
 
 // Scroll
 window.addEventListener('scroll', () => {
@@ -15,6 +16,36 @@ if (window.scrollY > 50) {
   header.classList.add('header-white');
 } else {
   header.classList.remove('header-white');
+}
+
+/* #Search Popup
+  ======================================================= */
+const headerSearchBtn = document.querySelector('.header-search a');
+const headerSearchClose = document.querySelector('.search-form .close');
+const headerSearchInput = document.querySelector('.search-form input');
+
+if (headerSearchBtn) {
+  headerSearchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    header.classList.add('show-search');
+    body.classList.add('no-scroll');
+  });
+
+  headerSearchClose.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    header.classList.remove('show-search');
+    body.classList.remove('no-scroll');
+  });
+
+  headerSearchInput.addEventListener('input', (e) => {
+    if (headerSearchInput.value !== '') {
+      header.classList.add('show-search-results');
+    } else {
+      header.classList.remove('show-search-results');
+    }
+  });
 }
 
 /* #Posts Slider
